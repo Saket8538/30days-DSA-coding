5489ba9dcb925f81303c835c831cd2431c305d1b
Saket Kumar <saket8538@gmail.com>
2026-06-23 23:19:26 +0530

    2.ThreeSum Problem

---
+++ ThreeSum.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class ThreeSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        Solution sol = new Solution();
+        List<List<Integer>> ans = sol.threeSum(nums);
+
+        for (List<Integer> triplet : ans) {
+            for (int val : triplet) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+                int total = nums[i] + nums[j] + nums[k];
+
+                if (total > 0) {
+                    k--;
+                } else if (total < 0) {
+                    j++;
+                } else {
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    while (j < k && nums[j] == nums[j - 1]) {
+                        j++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
