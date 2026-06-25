ac3283e906e105da86a83bb0e9f62755a9ccdc2e
Saket Kumar <saket8538@gmail.com>
2026-06-25 23:54:32 +0530

    Sort Colors Problem

---
+++ SortColors.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class SortColors {
+    public void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums, mid, high);
+                high--;
+            }
+        }
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        SortColors sol = new SortColors();
+        sol.sortColors(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
